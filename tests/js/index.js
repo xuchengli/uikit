cfd96a482f46abcbd03ea9c28583fd0051bb60d6
janschoenherr <jan@yootheme.com>
2017-02-13 16:30:44 +0100

    Fix icons not showing

--- tests/js/index.js
+++ tests/js/index.js
@@ -40,10 +40,8 @@ var style = styles[storage[key]] || styles.theme;
 // add style
 document.writeln(`<link rel="stylesheet" href="${dir !== 'rtl' ? style.file : style.file.replace('.css', '').concat('.rtl.css')}">`);
 
-// add javascripts
-['../dist/js/uikit-core.js']
-    .concat(components.map(name => `../dist/js/components/${name}.js`))
-    .forEach(file => document.writeln(`<script src="${file}"></script>`));
+// add javascript
+document.writeln(`<script src="../dist/js/uikit.js"></script>`);
 
 $(() => {
 
